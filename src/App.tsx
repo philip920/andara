dcc48e5ca8ccf6d1a3b8dc1010be8f2386fd74dc
philip920 <48444166+philip920@users.noreply.github.com>
2022-05-19 17:16:51 -0500

    add default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { Paper, Typography } from '@mui/material';
+import { defaultTheme } from './themes'
 import ConnectButton from './components/connect-button/ConnectButton';
 
 const App: React.FunctionComponent = () => {
 
   return (
-    <div className="App">
-      <ConnectButton />
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Paper className="App">
+        <Typography variant="h3" color="textPrimary">
+          Test
+        </Typography>
+        <Typography variant="h3" color="textSecondary">
+          Test
+        </Typography>
+        <ConnectButton />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.ts
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+export const defaultTheme = createTheme({
+  palette: {
+    text: {
+      primary: "rgb(255, 255, 255)",
+      secondary: "rgba(255, 255, 255, 0.7)"
+    }
+  },
+  typography: {
+    fontFamily: "Signika Negative",
+    fontWeightLight: 300,
+    fontWeightBold: 700,
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          background: "linear-gradient(180deg, #2A5859 0%, #124656 33.85%, #2C3B53 68.23%, #2D2C4B 100%);",
+          height: "100vh",
+          borderRadius: 0,
+        }
+      }
+    }
+  }
+});
