75f15b6a499c74950ab343f197cbba0b76b51128
philip920 <48444166+philip920@users.noreply.github.com>
2022-05-19 11:44:17 -0500

    add general types

---
+++ src/types/general.ts
@@ -0,0 +1,40 @@
+export type User = {
+  authWalletAdress: string;
+  username: string;
+  imageUrl: string;
+  contactList: Contact[];
+  useCases: UseCases;
+  contactInfo: UserContactInfo; 
+}
+
+export type UseCases = UseCase[];
+
+export type UseCase = {
+  name: string;
+  items: UseCaseItem[];
+}
+
+export type UseCaseItem = {
+  name: string;
+  imageUrl: string;
+  identifier: UseCaseItemIdentifier
+}
+
+export type UseCaseItemIdentifier = {
+  name: string;
+  value: string;
+}
+
+export type UserContactInfo = {
+  name: "contact";
+  items: UseCaseItem[];
+}
+
+export type Contact = {
+  authWalletAdress: string;
+  username: string;
+  imageUrl: string;
+  useCases: UseCase[];
+  contactInfo: UserContactInfo; 
+}
+
