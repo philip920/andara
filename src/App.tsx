aaa7711a730eb739b2a99889e55e4e7bdda3be26
philip920 <48444166+philip920@users.noreply.github.com>
2022-06-02 13:23:06 -0500

    add react-router and some basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "@self.id/framework";
 import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { defaultTheme } from "./themes";
 import ConnectButton from "./components/connect-button/ConnectButton";
 import SignUpFlow from "./components/sign-up/signup";
@@ -11,9 +12,13 @@ const App: React.FunctionComponent = () => {
     <Provider client={{ ceramic: "testnet-clay" }}>
       <ThemeProvider theme={defaultTheme}>
         <Paper>
-          <Grid sx={{ height: "100vh"}}>
-            <ConnectButton />
-            <SignUpFlow />
+          <Grid sx={{ height: "100vh" }}>
+            <BrowserRouter>
+              <Routes>
+                <Route index element={<ConnectButton />} />
+                <Route path="/signup" element={<SignUpFlow />} />
+              </Routes>
+            </BrowserRouter>
           </Grid>
         </Paper>
       </ThemeProvider>
