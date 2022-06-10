ff6d0023b182665355f8dae9229f7837c9400dfd
phil92 <philip.chan92@protonmail.com>
2022-06-10 13:33:51 -0500

    encrypt and write user object onSubmit signup form

--- src/components/sign-up/signup.tsx
+++ src/components/sign-up/signup.tsx
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Integration } from "lit-ceramic-sdk";
+import { useStore } from "../../store";
+import { useNavigate } from "react-router-dom";
+
 import { CenteredGrid } from "../common/styled-components";
 import { User } from "../../types/general";
+import ConnectButton from "../connect-button/ConnectButton";
 import StepOne from "./step-one/step-one";
 import StepTwo from "./step-two/step-two";
 import StepThree from "./step-three/step-three";
 import StepFour from "./step-four/step-four";
 import StepFive from "./step-five/step-five";
 import StepSix from "./step-six/step-six";
-import { useStore } from "../../store";
-
 
 const options = ["testoption1", "testoption2", "testoption3", "testoption4"];
 
 const SignUp: React.FunctionComponent = () => {
   const store = useStore((state) => state);
-  const [currentStep, setCurrentStep] = useState(1);
 
+  const litCeramicIntegration = new Integration(
+    "https://ceramic-clay.3boxlabs.com",
+    "ethereum"
+  );
+
+  const [currentStep, setCurrentStep] = useState(1);
   const [user, setUser] = useState<User>(store.user);
+
+  const navigate = useNavigate();
+
+  litCeramicIntegration.startLitClient(window);
 
   const handleNextClick = (): void => {
     setCurrentStep(currentStep + 1);
@@ -44,41 +56,69 @@ const SignUp: React.FunctionComponent = () => {
     });
   };
 
-  const handleSubmit = (): void => {
-    console.log("final user object:", user);
+  const handleSubmit = async () => {
+    await store.setUser(user);
+
+    const accessControlConditions = [
+      {
+        contractAddress: "",
+        standardContractType: "",
+        chain: "ethereum",
+        method: "",
+        parameters: [":userAddress"],
+        returnValueTest: {
+          comparator: "=",
+          value: user.authWalletAdress,
+        },
+      },
+    ];
+
+    const stringToEncrypt = JSON.stringify(user);
+
+    const response = litCeramicIntegration
+      .encryptAndWrite(stringToEncrypt, accessControlConditions)
+      .then((streamID: string) => console.log("streamID:", streamID));
+
+    navigate("/home");
   };
 
   console.log("user:", user);
 
   return (
-    <CenteredGrid>
-      <form onSubmit={handleSubmit}>
-        {currentStep === 1 && <StepOne onSignUpClick={handleNextClick} />}
-        {currentStep === 2 && (
-          <StepTwo onChange={handleInputChange} onNextClick={handleNextClick} />
-        )}
-        {currentStep === 3 && (
-          <StepThree
-            onChange={handleInputChange}
-            onNextClick={handleNextClick}
-          />
-        )}
-        {currentStep === 4 && (
-          <StepFour
-            options={options}
-            onChange={handleUseCaseChange}
-            onNextClick={handleNextClick}
-          />
-        )}
-        {currentStep === 5 && (
-          <StepFive
-            onChange={handleContactListChange}
-            onNextClick={handleNextClick}
-          />
-        )}
-        {currentStep === 6 && <StepSix />}
-      </form>
-    </CenteredGrid>
+    <>
+      <ConnectButton />
+      <CenteredGrid>
+        <form onSubmit={handleSubmit}>
+          {currentStep === 1 && <StepOne onSignUpClick={handleNextClick} />}
+          {currentStep === 2 && (
+            <StepTwo
+              onChange={handleInputChange}
+              onNextClick={handleNextClick}
+            />
+          )}
+          {currentStep === 3 && (
+            <StepThree
+              onChange={handleInputChange}
+              onNextClick={handleNextClick}
+            />
+          )}
+          {currentStep === 4 && (
+            <StepFour
+              options={options}
+              onChange={handleUseCaseChange}
+              onNextClick={handleNextClick}
+            />
+          )}
+          {currentStep === 5 && (
+            <StepFive
+              onChange={handleContactListChange}
+              onNextClick={handleNextClick}
+            />
+          )}
+          {currentStep === 6 && <StepSix />}
+        </form>
+      </CenteredGrid>
+    </>
   );
 };
 
