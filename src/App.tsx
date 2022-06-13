2dbe96f75967a7b5c8044fa90f35a105439234e5
phil92 <philip.chan92@protonmail.com>
2022-06-13 15:02:16 -0500

    add short-term todos

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,13 @@ import Home from "./components/home/home";
 
 import { Paper, Grid } from "@mui/material/";
 
+// TODO (short-term):
+// - add protected routes
+// - add localization
+// - add component level tests for sign-up flow
+// - add end-to-end tests for sign-up flow
+// - fix lit-ceramic-integration bug
+
 const App: React.FunctionComponent = () => {
   return (
     <Provider client={{ ceramic: "testnet-clay" }}>
